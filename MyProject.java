5fed7da83005412b6230e0fa5c5bff0a2a2a5bda
Supatsorn Poonsawat <supatsorn.supa@mail.kmutt.ac.th>
2022-03-26 15:18:57 +0700

    second committed

--- MyProject.java
+++ MyProject.java
@@ -15,6 +15,7 @@ public class MyProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello Worlds!");
         // TODO code application logic here
     }
     
